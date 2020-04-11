370a30f74574e92c13af54e64dee8788a5062e8f
hossainemruz <emruz@appscode.com>
2020-04-12 01:11:53 +0600

    journey begins...

---
+++ toha/static/js/navbar.js
@@ -0,0 +1,27 @@
+"use strict";
+
+(function ($) {
+  jQuery(document).ready(function () {
+
+    // change navbar style on scroll
+    // ==================================================
+    // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+    // $.onscroll = function() {scrollFunction()};
+    $(document).scroll(function () {
+      if ($(document).scrollTop() > 40) {
+        $('#top-navbar').removeClass('initial-navbar');
+        $('#top-navbar').addClass('final-navbar shadow');
+
+        $('#navbar-toggler').removeClass('navbar-dark');
+        $('#navbar-toggler').addClass('navbar-light');
+      } else {
+        $('#top-navbar').removeClass('final-navbar shadow');
+        $('#top-navbar').addClass('initial-navbar');
+
+        $('#navbar-toggler').removeClass('navbar-light');
+        $('#navbar-toggler').addClass('navbar-dark');
+      }
+    });
+  });
+
+})(jQuery);
