9ca1afca986acebf082616cfe5ab35ddb0606dfc
hossainemruz <emruz@appscode.com>
2020-06-13 01:52:23 +0600

    fix post-card height correction

--- static/assets/js/list.js
+++ static/assets/js/list.js
@@ -32,12 +32,12 @@ var isMobile = false, isTablet = false, isLaptop = false;
         let el = postCardHolder.children;
         let maxHeight = 0;
         for (let i = 0; i < el.length; i++) {
-          if (el[i].children[0].clientHeight > maxHeight) {
-            maxHeight = el[i].children[0].clientHeight;
+          if (el[i].children[1].clientHeight > maxHeight) {
+            maxHeight = el[i].children[1].clientHeight;
           }
         }
         for (let i = 0; i < el.length; i++) {
-          el[i].children[0].setAttribute("style", "min-height: " + maxHeight + "px;")
+          el[i].children[1].setAttribute("style", "min-height: " + maxHeight + "px;")
         }
       }
     }
