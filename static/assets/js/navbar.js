77d3c768439fd29e6d65c61c828161dfa56e4e14
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2020-07-16 02:43:30 +0200

    Fix problem related with performance

--- static/assets/js/navbar.js
+++ static/assets/js/navbar.js
@@ -8,6 +8,8 @@
     // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
     // $.onscroll = function() {scrollFunction()};
     $(document).scroll(function () {
+      const logo = $('#logo');
+      const source = logo.attr("src");
       if ($(document).scrollTop() > 40) {
         $('#top-navbar').removeClass('initial-navbar');
         $('#top-navbar').addClass('final-navbar shadow');
@@ -15,15 +17,19 @@
         $('#navbar-toggler').removeClass('navbar-dark');
         $('#navbar-toggler').addClass('navbar-light');
 
-        $('#logo').attr("src","assets/images/logo.png");
+        if (!source.includes("assets/images/logo.png")) {
+          logo.attr("src","assets/images/logo.png");
+        }
       } else {
         $('#top-navbar').removeClass('final-navbar shadow');
         $('#top-navbar').addClass('initial-navbar');
 
         $('#navbar-toggler').removeClass('navbar-light');
         $('#navbar-toggler').addClass('navbar-dark');
-
-        $('#logo').attr("src","assets/images/logo-inverted.png");
+        
+        if (!source.includes("assets/images/logo-inverted.png")) {
+          logo.attr("src","assets/images/logo-inverted.png");
+        }
       }
     });
   });
