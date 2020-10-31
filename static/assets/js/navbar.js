beb9d55b9599dd751149003ba36acf6262a79270
alex bezek <alex.bezek@cerner.com>
2020-10-31 11:35:23 -0400

    Fix the nav item collapse and spy functionality (#138)

--- static/assets/js/navbar.js
+++ static/assets/js/navbar.js
@@ -30,6 +30,15 @@
         $('#logo').attr("src", invertedLogo);
       }
     });
+
+    // Creates a click handler to collapse the navigation when
+    // anchors in the mobile nav pop up are clicked
+    var navMain = $(".navbar-collapse");
+    if (navMain) {
+      navMain.on("click", "a", null, function (e) {
+        $('.navbar-collapse').collapse('hide');
+      });
+    }
   });
 
 })(jQuery);
