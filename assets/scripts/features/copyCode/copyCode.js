8fe6d54cac934ab199f2cad0d5a7faa7b75db5e7
BernatBC <bernat.borrasc@gmail.com>
2024-08-31 14:14:18 +0200

    External package not needed

--- assets/scripts/features/copyCode/copyCode.js
+++ assets/scripts/features/copyCode/copyCode.js
@@ -1,17 +1,4 @@
-if (navigator && navigator.clipboard) {
-  addCopyButtons(navigator.clipboard)
-} else {
-  const script = document.createElement('script')
-  script.src =
-    'https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js'
-  script.integrity = 'sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94='
-  script.crossOrigin = 'anonymous'
-  script.onload = function () {
-    addCopyButtons(clipboard)
-  }
-
-  document.body.appendChild(script)
-}
+addCopyButtons(navigator.clipboard)
 
 function addCopyButtons(clipboard) {
   document.querySelectorAll('pre > code').forEach(function (codeBlock) {
