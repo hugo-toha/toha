562c783a2723a13675d88dc43f01c14e630f1de8
hossainemruz <hossainemruz@gmail.com>
2024-09-07 03:02:49 +0600

    Update button style + add feedback on click
    Signed-off-by: hossainemruz <hossainemruz@gmail.com>

--- assets/scripts/features/copyCode/copyCode.js
+++ assets/scripts/features/copyCode/copyCode.js
@@ -2,9 +2,9 @@ addCopyButtons(navigator.clipboard)
 
 function addCopyButtons(clipboard) {
   document.querySelectorAll('pre > code').forEach(function (codeBlock) {
-    const button = document.createElement('a')
+    const button = document.createElement('button')
     button.title = "Copy"
-    button.className = 'copy-code-button'
+    button.className = 'copy-code-button btn btn-sm'
     button.innerHTML = "<i class='fa-regular fa-copy'></i>"
 
     button.addEventListener('click', function () {
