56787e09634ff3428240da8a43f5a1b18fa891a1
David G. Simmons <santafen@mac.com>
2024-10-21 16:51:16 -0400

    oops!

--- assets/scripts/core/theme-scheme.js
+++ assets/scripts/core/theme-scheme.js
@@ -1,6 +1,4 @@
 let theme = localStorage.getItem('theme-scheme') || localStorage.getItem('darkmode:color-scheme') || 'light'
-const b = 'bollocks!';
-console.log(b);
 if (theme === 'system') {
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     theme = 'dark'
