faec25703ceba5d933cc44bb21f299f12b04185d
Sharwin24 <sharwin24@gmail.com>
2025-06-29 19:14:07 -0500

    Script to obtain resume from local repo and save as PDF

---
+++ getResume.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp "/home/sharwin/Resume/main.pdf" "/home/sharwin/sharwin24.github.io/SharwinPatilResume.pdf"
+echo -e "Resume updated from \e[3m/home/sharwin/Resume\e[0m as \e[3mSharwinPatilResume.pdf\e[0m"
