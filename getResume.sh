b168b11902409378e7b0ce995858df7fce81df96
Sharwin24 <sharwin24@gmail.com>
2025-06-29 19:17:58 -0500

    Created new content folder and added resume to static folder

--- getResume.sh
+++ getResume.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp "/home/sharwin/Resume/main.pdf" "/home/sharwin/sharwin24.github.io/SharwinPatilResume.pdf"
+cp "/home/sharwin/Resume/main.pdf" "/home/sharwin/sharwin24.github.io/static/files/SharwinPatilResume.pdf"
 echo -e "Resume updated from \e[3m/home/sharwin/Resume\e[0m as \e[3mSharwinPatilResume.pdf\e[0m"
